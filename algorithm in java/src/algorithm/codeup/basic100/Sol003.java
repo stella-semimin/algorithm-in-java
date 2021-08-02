bc94a2bdf765fd6af60fef9ff08d3aecd8c3a3d4
DESKTOP-I8D5K56\semi <smmin0402@gmail.com>
2021-08-02 22:42:16 +0900

    003

---
+++ algorithm in java/src/algorithm/codeup/basic100/Sol003.java
@@ -0,0 +1,10 @@
+package algorithm.codeup.basic100;
+
+public class Sol003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello\nWorld");
+	}
+
+}
