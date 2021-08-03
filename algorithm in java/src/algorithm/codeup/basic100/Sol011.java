91214a24521e13a7c2b7391b2c3caf91a9580b74
DESKTOP-I8D5K56\semi <smmin0402@gmail.com>
2021-08-03 09:38:52 +0900

    sol10-17

---
+++ algorithm in java/src/algorithm/codeup/basic100/Sol011.java
@@ -0,0 +1,16 @@
+package algorithm.codeup.basic100;
+
+import java.util.Scanner;
+
+public class Sol011 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		char a = scanner.next().charAt(0);
+		System.out.printf("%s",a);
+		
+		scanner.close();
+	}
+
+
+}
--- algorithm in java/src/algorithm/codeup/basic100/Sol013.java
+++ algorithm in java/src/algorithm/codeup/basic100/Sol013.java
@@ -2,13 +2,15 @@ package algorithm.codeup.basic100;
 
 import java.util.Scanner;
 
-public class Sol009 {
+public class Sol013 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int num = scanner.nextInt();
-		System.out.println(num);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.printf("%d %d", a, b);
 		
+		scanner.close();
 	}
 
 }
